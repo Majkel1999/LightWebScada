46a566b1000c5642d1bdbdd001f36084547c030f
Michał Kaleta <mkaleta1999@gmail.com>
2021-07-05 23:42:17 +0200

    Dodanie projektu biblioteki wspólnej

---
+++ DataRegisters/DataSet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DataRegisters
+{
+    public class DataSet
+    {
+        public List<DiscreteRegister> CoilRegisters { get; set; }
+        public List<DiscreteRegister> DiscreteInputs { get; set; }
+        public List<ValueRegister> InputRegisters { get; set; }
+        public List<ValueRegister> HoldingRegisters { get; set; }
+    }
+}
