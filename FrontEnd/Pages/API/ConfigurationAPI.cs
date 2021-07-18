f297a1a57513f60f29e822b0e7d545efed154e45
Michal Kaleta <mkaleta1999@gmail.com>
2021-07-18 22:35:57 +0200

    Dodane zwracanie list konfiguracji po apikey

--- FrontEnd/Pages/API/ConfigurationAPI.cs
+++ FrontEnd/Pages/API/ConfigurationAPI.cs
@@ -1,6 +1,8 @@
 ﻿using FrontEnd.Areas.Organizations.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace FrontEnd.Pages.API
@@ -17,6 +19,11 @@ namespace FrontEnd.Pages.API
 
         [HttpGet("{apikey}")]
         public async Task<ActionResult<List<ClientConfigEntity>>> GetConfigurations(string apikey) {
+            var organization = await m_organizationContext.Organizations.FirstAsync(o => o.ApiKey == apikey);
+            if(organization.ApiKey == apikey){
+                var configurations = await m_organizationContext.Configurations.Where(c => c.OrganizationId == organization.OrganizationId).ToListAsync();
+                return configurations;
+            }
             return NoContent();
         }
     }
