42fa439b93759c6e524fa39df67c8b62eb36d35e
Michał Kaleta <mkaleta1999@gmail.com>
2021-07-29 21:31:40 +0200

    Dodane klasy odpowiedzialne za widoki w bazie

---
+++ FrontEnd/Areas/Views/Database/ViewObject.cs
@@ -0,0 +1,9 @@
+namespace FrontEnd.Areas.Organizations.Data
+{
+    public class ViewObject
+    {
+        public int Id { get; set; }
+        public int OrganizationId { get; set; }
+        public string ViewJson { get; set; }
+    }
+}
