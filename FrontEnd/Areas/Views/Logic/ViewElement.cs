4029e6f8d74aa02e0ddb8fcb53ad59b2a5d8b39e
Michał Kaleta <mkaleta1999@gmail.com>
2021-10-16 17:54:23 +0200

    Added ClientId Configuration to modal

--- FrontEnd/Areas/Views/Logic/ViewElement.cs
+++ FrontEnd/Areas/Views/Logic/ViewElement.cs
@@ -10,12 +10,14 @@ namespace FrontEnd.Areas.Organizations.Data
 
         [JsonProperty] private RegisterType m_registerType;
         [JsonProperty] private int m_registerAddress;
+        [JsonProperty] private int m_clientId;
         [JsonIgnore] private int m_value;
         [JsonIgnore] private List<RegisterFrame> m_initialData;
 
         [JsonIgnore] public RegisterType RegisterType => m_registerType;
         [JsonIgnore] public int RegisterAddress => m_registerAddress;
         [JsonIgnore] public int Value => m_value;
+        [JsonIgnore] public int ClientId => m_clientId;
         [JsonIgnore] public List<RegisterFrame> InitialData => m_initialData;
 
         public void SetRegisterAddress(int address) => m_registerAddress = address;
@@ -23,6 +25,8 @@ namespace FrontEnd.Areas.Organizations.Data
 
         public void SetValue(int value) => m_value = value;
         public void SetValue(bool value) => m_value = value ? 1 : 0;
+
+        public void SetClientId(int id) => m_clientId = id;
 
         public void SetInitialData(List<RegisterFrame> dataSets)
         {
