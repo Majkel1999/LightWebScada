f9199c9e0c3a7ba85124a4a562ddc2c9769d59bb
Michał Kaleta <mkaleta1999@gmail.com>
2021-10-23 14:11:41 +0200

    Automated deploy scripts

--- FrontEnd/deploy.sh
+++ FrontEnd/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+ssh root@maluch.mikr.us -p 10104 'rm -rf /var/scada/front'
 dotnet publish -c Release -o linux --runtime linux-x64 --self-contained false
 scp -P 10104 -r ./linux root@maluch.mikr.us:/var/scada/front
+ssh root@maluch.mikr.us -p 10104 'chown -R www /var/scada/front; /root/restartscada.sh'
