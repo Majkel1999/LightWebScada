2015cd0f8361434b7bade4154d3a4988cda2f02e
Michał Kaleta <mkaleta1999@gmail.com>
2021-11-01 13:42:44 +0100

    Added title to each element

--- FrontEnd/Areas/Organizations/Views/Logic/ViewElement.cs
+++ FrontEnd/Areas/Organizations/Views/Logic/ViewElement.cs
@@ -13,20 +13,24 @@ namespace FrontEnd.Areas.Organizations.Data
         [JsonProperty] private RegisterType m_registerType;
         [JsonProperty] private int m_registerAddress;
         [JsonProperty] private int m_clientId = 1;
+        [JsonProperty] private string m_title;
         [JsonIgnore] private RegisterFrame m_frameValue;
         [JsonIgnore] private bool m_updated = false;
         [JsonIgnore] private List<RegisterFrame> m_initialData;
 
+
+        [JsonIgnore] public DateTime TimeStamp => m_frameValue?.Timestamp.ToLocalTime() ?? DateTime.Now;
+        [JsonIgnore] public List<RegisterFrame> InitialData => m_initialData;
         [JsonIgnore] public RegisterType RegisterType => m_registerType;
+        [JsonIgnore] public string Title => m_title;
         [JsonIgnore] public int RegisterAddress => m_registerAddress;
         [JsonIgnore] public int Value => m_frameValue?.Value ?? 0;
-        [JsonIgnore] public DateTime TimeStamp => m_frameValue?.Timestamp ?? DateTime.Now;
         [JsonIgnore] public int ClientId => m_clientId;
-        [JsonIgnore] public List<RegisterFrame> InitialData => m_initialData;
 
         public void SetRegisterAddress(int address) => m_registerAddress = address;
         public void SetRegisterType(RegisterType registerType) => m_registerType = registerType;
         public void SetClientId(int id) => m_clientId = id;
+        public void SetTitle(string title) => m_title = title;
 
         public void SetValue(RegisterFrame value)
         {
@@ -49,7 +53,7 @@ namespace FrontEnd.Areas.Organizations.Data
         public void SetInitialData(List<RegisterFrame> dataSets)
         {
             m_initialData = dataSets;
-            m_frameValue = dataSets.OrderByDescending(x=>x.Timestamp).FirstOrDefault();
+            m_frameValue = dataSets.OrderByDescending(x => x.Timestamp).FirstOrDefault();
         }
     }
 }
