007738f4b51c4d7f443ce9a96ede045b8868a55f
Michał Kaleta <mkaleta1999@gmail.com>
2021-11-01 17:13:23 +0100

    Added specific parameters to each control element

--- FrontEnd/Areas/Organizations/Views/Logic/ViewElement.cs
+++ FrontEnd/Areas/Organizations/Views/Logic/ViewElement.cs
@@ -9,22 +9,24 @@ namespace FrontEnd.Areas.Organizations.Data
 {
     public class ViewElement
     {
-        public ViewType ViewType;
-
+        [JsonProperty] private ViewType m_viewType;
         [JsonProperty] private RegisterType m_registerType;
+        [JsonProperty] private Dictionary<string, object> m_parameters;
         [JsonProperty] private int m_registerAddress;
         [JsonProperty] private int m_clientId = 1;
         [JsonProperty] private string m_title;
         [JsonProperty] private string m_expression;
+
         [JsonIgnore] private List<DisplayedData> m_initialData;
         [JsonIgnore] private RegisterFrame m_frameValue;
         [JsonIgnore] private double m_displayedValue;
         [JsonIgnore] private bool m_updated = false;
 
-
         [JsonIgnore] public DateTime TimeStamp => m_frameValue?.Timestamp.ToLocalTime() ?? DateTime.Now;
         [JsonIgnore] public List<DisplayedData> InitialData => m_initialData;
+        [JsonIgnore] public Dictionary<string, object> Parameters => m_parameters;
         [JsonIgnore] public RegisterType RegisterType => m_registerType;
+        [JsonIgnore] public ViewType ViewType => m_viewType;
         [JsonIgnore] public string Title => m_title;
         [JsonIgnore] public string Expression => m_expression;
         [JsonIgnore] public int RegisterAddress => m_registerAddress;
@@ -36,6 +38,37 @@ namespace FrontEnd.Areas.Organizations.Data
         public void SetClientId(int id) => m_clientId = id;
         public void SetTitle(string title) => m_title = title;
         public void SetMathExpression(string expression) => m_expression = expression;
+
+        public ViewElement(ViewType type)
+        {
+            m_viewType = type;
+            CreateParametersDictionary(type);
+        }
+
+        private void CreateParametersDictionary(ViewType type)
+        {
+            m_parameters = new Dictionary<string, object>();
+            switch (type)
+            {
+                case ViewType.Text:
+                    m_parameters.Add("Prefix", "");
+                    m_parameters.Add("Suffix", "");
+                    break;
+                case ViewType.Gauge:
+                    m_parameters.Add("Minimum", 0);
+                    m_parameters.Add("Maximum", 10000);
+                    m_parameters.Add("Step", 1000);
+                    break;
+                case ViewType.Signal:
+                    break;
+                case ViewType.LineChart:
+                    m_parameters.Add("Minimum", 0);
+                    m_parameters.Add("Maximum", 10000);
+                    m_parameters.Add("Step", 1000);
+                    m_parameters.Add("Name", "Value");
+                    break;
+            }
+        }
 
         public void SetValue(RegisterFrame value)
         {
